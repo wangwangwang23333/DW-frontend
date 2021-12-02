3ad8eba733a7d4096d0017b42aabd906164e9595
汪汪汪 <1209497940@qq.com>
2021-12-02 19:37:18 +0800

    增加了条形图

--- src/main.js
+++ src/main.js
@@ -15,6 +15,8 @@ import router from './router'
 import '@/icons' // icon
 import '@/permission' // permission control
 
+import VCharts from 'v-charts' // vchart
+
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
@@ -32,6 +34,8 @@ if (process.env.NODE_ENV === 'production') {
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
+
+Vue.use(VCharts)
 
 Vue.config.productionTip = false
 
--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -118,7 +118,7 @@
             <el-input-number v-model="form.movieMaxScore" :precision="2" :step="0.01" :max="5" :min="form.movieMinScore" />
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" >Create</el-button>
+            <el-button type="primary" @click="searchMovie">Create</el-button>
             <el-button @click="onCancel">Cancel</el-button>
           </el-form-item>
         </el-form>
@@ -126,24 +126,35 @@
       <el-col :span="1">
         <el-divider direction="vertical"></el-divider>
       </el-col>
-      <el-col :span="8">
+      <el-col :span="10">
         <el-tabs v-model="activeName" @tab-click="handleClick">
           <el-tab-pane label="查询结果" name="first">
             用户管理
+
           </el-tab-pane>
           <el-tab-pane label="数据血缘" name="second">
             配置管理
           </el-tab-pane>
-          <el-tab-pane label="速度比较" name="third">
-            配置管理
+          <el-tab-pane label="速度对比" name="third">
+
+            <ve-histogram
+              class="myve"
+              :data="chartData"
+              :settings="vchartsConfig.setting"
+              :extend="vchartsConfig.extend"
+              width="40vw"
+            ></ve-histogram>  
           </el-tab-pane>
        </el-tabs>
+
+       
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
+import "echarts/lib/component/title";
 /* eslint-disable */
 export default {
   filters: {
@@ -215,6 +226,148 @@ export default {
           },
         ]
       },
+      
+      // 速度比较图
+      vchartsConfig: {
+        setting:{
+          // 别称
+          labelMap: {
+            'type': '数据库',
+            'software': '软件',
+            'speed': '速度',
+          },
+        },
+        extend: {
+          title:{
+            show:true,
+            text:'检索电影',
+            subtext:'查找电影名为[HarryPotter]的所有电影',
+            // textAlign:'center',
+          },
+          // 图标顶部的标题及按钮
+          legend:{
+            show:false,
+          },
+          // backgroundColor:'red',//整个组件的背景颜色
+          //X轴线
+          xAxis: {
+            // name: "地区",
+            type:'category',
+            show:true,
+            // 坐标轴轴线
+            axisLine:{
+              show:false,
+            },
+            // 坐标轴刻度
+            axisTick:{
+              show:false,
+            },
+            // 坐标轴每项的文字
+            axisLabel:{
+              showMaxLabel:true,
+              showMinLabel:true,
+              color:'#3a3a3a',
+              rotate:0, //刻度文字旋转，防止文字过多不显示
+              margin:8,//文字离x轴的距离
+              boundaryGap:true,
+              // backgroundColor:'#0f0',
+              formatter:(v)=>{
+                // console.log('x--v',v)
+                return v
+              },
+            },
+            // X轴下面的刻度小竖线
+            axisTick:{
+              show:false,
+              alignWithLabel:true,//axisLabel.boundaryGap=true时有效
+              interval:0,
+              length:4,//长度
+            },
+            // x轴对应的竖线
+            splitLine: {
+              show: false,
+              interval: 0,
+              lineStyle:{
+                color:'red',
+                backgroundColor:'red',
+              }
+            }
+          },
+          yAxis: {
+            show:true,
+            offset:0,
+            // 坐标轴轴线
+            axisLine:{
+              show:false,
+            },
+            // x轴对应的竖线
+            splitLine: {
+              show: true,
+            },
+            // 坐标轴刻度
+            axisTick:{
+              show:false,
+            },
+            boundaryGap:true,
+            axisLabel:{
+              color:'#3a3a3a',
+              formatter:(v)=>{
+                if (v==0){
+                  return v;
+                }
+                return v+' ms'
+            },
+            },
+            
+          },
+          
+          // 滚动组件参数
+          dataZoom:[
+            {
+              type: 'inside',
+              show: true,
+              xAxisIndex: [0],
+              startValue: 0,
+              endValue: 4,
+              zoomLock:true,//阻止区域缩放
+            }
+          ],
+
+
+
+          // 每个柱子
+          series(v) {
+            // console.log("v", v);
+            // 设置柱子的样式
+            v.forEach(i => {
+              console.log("series", i);
+              i.barWidth = 20;
+              i.itemStyle={
+                barBorderRadius:[10,10,10,10],
+                borderWidth:20,
+              };
+              i.label={
+                color:'#666',
+                show:true,
+                position:'top',
+                // backgroundColor:'yellow',
+              };
+
+            });
+            return v;
+          },
+        }
+      },
+      // v-chats列表数据
+      chartData: {
+        columns: ["type","speed"],
+        rows: [
+          { "type":"关系型数据库","software": "mysql", "speed": 570 },
+          { "type":"分布式数据库","software": "hive", "speed": 800 },
+          { "type":"图数据库","software": "neo4j", "speed": 1540 },
+        ],
+      },
+      
       directorInputVisible:false,
       directorInputValue:'',
       mainActorInputVisible:false,
@@ -237,6 +390,9 @@ export default {
     handleClick(tab, event) {
       console.log(tab, event);
     },
+    searchMovie(){
+      this.vchartsConfig.extend.title.subtext="查找电影"
+    },
 
     /**
      * 下面是处理标签的函数 
@@ -258,6 +414,7 @@ export default {
           message: '请输入有效的导演名称！',
           type: 'warning'
         })
+        this.directorInputVisible=false;
         return;
       }
       this.form.movieDirectors.push(inputValue.replace(/^\s*|\s*$/g,""));
@@ -284,6 +441,7 @@ export default {
           message: '请输入有效的主演名称！',
           type: 'warning'
         })
+        this.mainActorInputVisible=false;
         return;
       }
       this.form.movieMainActors.push(inputValue.replace(/^\s*|\s*$/g,""));
@@ -311,6 +469,7 @@ export default {
           message: '请输入有效的演员名称！',
           type: 'warning'
         })
+        this.actorInputVisible=false;
         return;
       }
       this.form.movieActors.push(inputValue.replace(/^\s*|\s*$/g,""));
