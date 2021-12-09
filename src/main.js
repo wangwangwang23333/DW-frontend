de227c4bd9ba5c9115dc3ac4018d0f1bcaefa318
汪汪汪 <1209497940@qq.com>
2021-12-09 23:06:32 +0800

    增加桑基图

--- src/main.js
+++ src/main.js
@@ -30,6 +30,9 @@ if (process.env.NODE_ENV === 'production') {
   mockXHR()
 }
 
+import echarts from 'echarts'
+Vue.prototype.$echarts = echarts
+
 // set ElementUI lang to EN
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -1,19 +1,104 @@
 <template>
   <div class="app-container">
-    
+    <div id="myChart" style="width: 95vw;height: 50vh;"></div>
+    <el-divider></el-divider>
   </div>
 </template>
 
 <script>
+
 export default {
   data() {
     return {
-      
+
     }
   },
   methods: {
    
-  }
+  },
+  created(){
+
+  },
+  mounted(){
+    let myChart = this.$echarts.init(document.getElementById('myChart'))
+
+    let option = {
+      tooltip: {
+        trigger: 'item',
+        triggerOn: 'mousemove'
+      },
+      
+      series: {
+        type: 'sankey',
+        layout: 'none',
+        emphasis: {
+          focus: 'adjacency'
+        },
+        selectedMode:"single",
+        select:{
+          show:true,
+          position:'top',
+        },
+        lineStyle: {
+          color: 'source',
+          curveness: 0.5
+        },
+        data: [
+          {
+            name: '评论数据'
+          },
+          {
+            name: 'Amazon数据'
+          },
+          {
+            name: '404数据'
+          },
+          {
+            name: '电影数据'
+          },
+          {
+            name: '非电影数据'
+          },
+          {
+            name:"合并冲突后的数据"
+          }
+
+        ],
+        links: [
+          {
+            source: '评论数据',
+            target: 'Amazon数据',
+            value: 5000
+          },
+          {
+            source: '评论数据',
+            target: '404数据',
+            value: 5
+          },
+          {
+            source: 'Amazon数据',
+            target: '电影数据',
+            value: 3500
+          },
+          {
+            source: 'Amazon数据',
+            target: '非电影数据',
+            value: 500
+          },
+          {
+            source: '电影数据',
+            target: '合并冲突后的数据',
+            value: 3000
+          },
+        ]
+      }
+    }
+    // 绘制图表
+    myChart.setOption(option);
+    myChart.on('click',param=>{
+      console.log(param.data)
+    })
+  },
 }
 </script>
 
